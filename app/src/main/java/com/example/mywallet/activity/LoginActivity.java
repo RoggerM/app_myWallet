ef67f5c7e8f83150826de4b611f452b554967d3b
Rogger Marlon <roggermarlon@hotmail.com>
2020-07-17 09:37:26 -0300

    initial commit

---
+++ app/src/main/java/com/example/mywallet/activity/LoginActivity.java
@@ -0,0 +1,111 @@
+package com.example.mywallet.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.mywallet.R;
+import com.example.mywallet.config.ConfiguracaoFirebase;
+import com.example.mywallet.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText campoEmail, campoSenha;
+    private Button buttonEntrar;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        campoEmail = findViewById(R.id.editEmail);
+        campoSenha = findViewById(R.id.editSenha);
+        buttonEntrar = findViewById(R.id.buttonEntrar);
+
+        buttonEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String textoEmail = campoEmail.getText().toString();
+                String textoSenha = campoSenha.getText().toString();
+
+
+                if (!textoEmail.isEmpty()){
+                    if (!textoSenha.isEmpty()){
+
+                        usuario = new Usuario();
+                        usuario.setEmail( textoEmail );
+                        usuario.setSenha( textoSenha );
+                        validarLogin();
+
+                    }else {
+                        Toast.makeText(LoginActivity.this, "Preencha a senha",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                }else {
+                    Toast.makeText(LoginActivity.this, "Preencha o email",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+
+    public void validarLogin(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.signInWithEmailAndPassword(
+                    usuario.getEmail(),
+                    usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()){
+
+                    abrirTelaPrincipal();
+
+                }else{
+
+                    String excecao = "";
+                    try {
+                        throw task.getException();
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        excecao = "Usuário não cadastrado";
+                    }catch (FirebaseAuthInvalidUserException e){
+                        excecao = "Usuário ou senha incorreta";
+                    }catch (Exception e){
+                        excecao = "Erro ao entrar" + e.getMessage();
+                        e.printStackTrace();
+                    }
+
+                    Toast.makeText(LoginActivity.this, excecao, Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+    public void abrirTelaPrincipal(){
+        startActivity(new Intent(this, PrincipalActivity.class));
+        finish();
+    }
+
+}
---
+++ app/src/main/java/com/example/mywallet/activity/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.mywallet.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.mywallet.R;
+import com.example.mywallet.activity.CadastroActivity;
+import com.example.mywallet.activity.LoginActivity;
+import com.example.mywallet.config.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+
+public class MainActivity extends IntroActivity {
+
+    private FirebaseAuth autenticacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Organize suas contas de onde estiver")
+                .description("Simples, fácil de usar e grátis")
+                .image(R.drawable.um)
+                .background(android.R.color.white)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Saiba para onde está indo seu dinheiro")
+                .description("Categorizando seus lançamentos e vendo o destino do cada centavo")
+                .image(R.drawable.dois)
+                .background(android.R.color.white)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Nunca mais esqueça de pagar uma conta")
+                .description("Receba alertas quando quiser")
+                .image(R.drawable.tres)
+                .background(android.R.color.white)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Tudo organizado no celular ou computador")
+                .description("")
+                .image(R.drawable.quatro)
+                .background(android.R.color.white)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                                .background(android.R.color.white)
+                                .fragment(R.layout.intro_layout)
+                                .canGoForward(false)
+                                .build()
+        );
+
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        verificarUsuarioLogado();
+    }
+
+    public void btEntrar(View view){
+       startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void  btCadastrar(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
+    }
+
+    public void verificarUsuarioLogado(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        //autenticacao.signOut();
+        if (autenticacao.getCurrentUser() != null){
+            abrirTelaPrincipal();
+            finish();
+        }
+    }
+
+    public void abrirTelaPrincipal(){
+        startActivity(new Intent(this, PrincipalActivity.class));
+    }
+
+
+}
