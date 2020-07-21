e97cbac8f006f74cc1aa6fec8f688fd5465c6b49
Rogger Marlon <roggermarlon@hotmail.com>
2020-07-21 03:02:22 -0300

    Finish register screen

---
+++ app/src/main/java/com/example/mywallet/helper/DateUtil.java
@@ -0,0 +1,12 @@
+package com.example.mywallet.helper;
+
+import java.text.SimpleDateFormat;
+
+public class DateUtil {
+
+    public static String dataAtual(){
+        long date = System.currentTimeMillis();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("d/M/yyyy");
+    }
+
+}
